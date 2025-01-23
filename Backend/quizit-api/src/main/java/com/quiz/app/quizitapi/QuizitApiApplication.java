b6948c06628f0ef804f6b27c2f6e414fb2a9897b
NinaMadu <ninadamaduwantha@gmail.com>
2025-01-23 07:23:51 +0530

    Initial commit

---
+++ Backend/quizit-api/src/main/java/com/quiz/app/quizitapi/QuizitApiApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.app.quizitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizitApiApplication.class, args);
+	}
+
+}
